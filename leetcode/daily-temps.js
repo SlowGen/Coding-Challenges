729c3fefddf1da831a42b6c662b46ea76f918113
slowgen <kalibobailey@gmail.com>
2021-04-27 12:03:36 -0400

    Updated: added new approaches, daily temp - leetcode

--- leetcode/daily-temps.js
+++ leetcode/daily-temps.js
@@ -16,7 +16,7 @@ Each temperature will be an integer in the range [30, 100].
 //reset pointer to i+1 for each iteration
 //results pointer - i or 0 (if end is reached and no answer)
 
-var dailyTemperatures = function(T) {
+var dailyTemperaturesITERATION = function(T) {
     const results = []
     
     for (let i=0; i<T.length; i++) {
@@ -33,5 +33,54 @@ var dailyTemperatures = function(T) {
     }
     return results
 };
+
+//Map approach is very similar to stack approach, but less efficient
+
+function dailyTemperaturesMAP(T) {
+    const results = new Array(T.length)
+
+    const rise = new Map()
+    for (let i=0; i<T.length -1; i++) {
+        if (T[i+1] > T[i]) rise.set(i+1, T[i+1])
+    }
+    rise.set(T.length, Infinity)
+
+    for (let i=0; i<T.length; i++) {
+        let nextRise = rise.keys().next().value //index
+        if (nextRise === T.length) results[i] = 0
+        else {
+            if (nextRise === i) {
+                rise.delete(nextRise)
+                nextRise = rise.keys().next().value //index
+            }
+            for (const increases of rise) {
+                if (increases[1] > T[i]) {
+                    results[i] = increases[1] < Infinity ? increases[0] - i : 0
+                    break;
+                }
+            }
+            
+        }
+    }
+    return results
+}
+
+
+/**
+ * stack approach, same exact idea as map approach, but goes backwards..which is the key to speed for this
+ * particular problem
+ */
+
+function dailyTemperaturesSTACK(T) {
+    const results = new Array(T.length)
+
+    const stack = []
+    for (let i=T.length -1; i>=0; i--) {
+        while (stack.length && T[i] >= T[stack[stack.length-1]]) stack.pop()
+        results[i] = !stack.length ? 0 : stack[stack.length-1] - i
+        stack.push(i)
+    }
+    return results
+}
 
 //https://leetcode.com/problems/daily-temperatures/
